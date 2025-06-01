2d20ec2fbf0e31540587a248c44e27746677f50c
dhuruv bansal <dhuruvbansal99@gmail.com>
2025-06-01 16:09:09 +0530

    fixed middle ware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,15 +3,15 @@ import { cookies } from "next/headers";
 
 export async function middleware(req: Request) {
   const cookieStore = await cookies();
-  // try {
-  //   const refreshToken = cookieStore.get("refreshToken");
-  //   if (!refreshToken) {
-  //     return NextResponse.redirect(new URL("/login", req.url));
-  //   }
-  // } catch (err) {
-  //   console.log(err);
-  //   console.log("error");
-  // }
+  try {
+    const refreshToken = cookieStore.get("refreshToken");
+    if (!refreshToken) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
+  } catch (err) {
+    console.log(err);
+    console.log("error");
+  }
   return NextResponse.next();
 }
 export const config = {
